7090fe13cf39988a86adbcb4d3ec8e1d30fc6e1e
kangzixiang <kangzixiang@foxmail.com>
2023-10-26 17:26:48 +0800

    Get image path from prameter

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,22 @@
 #include "mtcnn.h"
 #include <time.h>
 
-int main()
+int main(int argc, char** argv)
 {
-    Mat image = imread("res/4.jpg");
+    char* imagepath = argv[1];
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        imagepath = "res/4.jpg";
+    }
+
+    Mat image = imread(imagepath);
     mtcnn find(image.rows, image.cols);
     clock_t start;
     start = clock();
     find.findFace(image);
     imshow("result", image);
-    imwrite("res/result.jpg",image);
+    imwrite("result.jpg",image);
     start = clock() -start;
     cout<<"time is  "<<start/10e3<<endl;
     // Mat image;
